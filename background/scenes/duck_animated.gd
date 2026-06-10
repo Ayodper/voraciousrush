77a141c7a2e358a664adabb68770dcb6ddf5e06c
Ayodper <24240@burnside.school.nz>
2026-06-11 11:24:49 +1200

    idle animation before space is pressed

--- background/scenes/duck_animated.gd
+++ background/scenes/duck_animated.gd
@@ -7,15 +7,18 @@ const JUMP_SPEED = -1800
 func _physics_process(delta):
 	velocity.y+= GRAVITY * delta
 	if is_on_floor():
-		$runcol.disabled = false
-		if Input.is_action_pressed("ui_accept"):
-			velocity.y=JUMP_SPEED
-		
-		elif Input.is_action_pressed("ui_down"):
-			$AnimatedSprite2D.play("duck")
-			$runcol.disabled = true
+		if not get_parent().game_running:
+			$AnimatedSprite2D.play("idle")
 		else:
-			$AnimatedSprite2D.play("run")
+			$runcol.disabled = false
+			if Input.is_action_pressed("ui_accept"):
+				velocity.y=JUMP_SPEED
+			
+			elif Input.is_action_pressed("ui_down"):
+				$AnimatedSprite2D.play("duck")
+				$runcol.disabled = true
+			else:
+				$AnimatedSprite2D.play("run")
 	else:
 		$AnimatedSprite2D.play("jump")
 		
